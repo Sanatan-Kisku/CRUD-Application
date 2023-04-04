66cff11e40fcdc332c3da706933b0c9a035a984e
Sanatan Kisku <sanatankisku@gmail.com>
2023-04-04 20:22:09 +0530

    API integration

---
+++ server/model/model.js
@@ -0,0 +1,51 @@
+const { time } = require('console');
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  booking_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  location_id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  drone_shot_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  created_time: {
+    type: time,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    required: true,
+  }
+
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
